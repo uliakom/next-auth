b659b2d9ea96ba13c322898464de340cfd11bc76
Uliana Komarynska <uliakom@gmail.com>
2025-06-24 22:57:17 +0300

    added middleware

---
+++ routes.ts
@@ -0,0 +1,27 @@
+/**
+ * These routes are public, do not require authentication
+ * @type{string[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * These routes are used for authentication and will reirect user to /settings
+ * @type{string[]}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for APi authentication routs.Routs that start from this prefix are used for API
+ * @type{string}
+ */
+
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after logging in
+ * @type{string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
