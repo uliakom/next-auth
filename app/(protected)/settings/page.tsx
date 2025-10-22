d987d581c63f51c4c21ae14f949e65a12dbf7817
Uliana Komarynska <uliakom@gmail.com>
2025-10-22 15:21:43 +0300

    added sign out

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,8 +1,23 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+
+// signOut only for server components
 
 const SettingsPage = async () => {
   const session = await auth();
-    return <div><p>Settings Page</p></div>;
+  return (
+    <div>
+      {/* {JSON.stringify(session)} */}
+      <p>Name: {session?.user?.name}</p>
+      <p>Email: {session?.user?.email}</p>
+      <p>Settings Page</p>
+      <form action={async () => {
+        "use server";
+        await signOut();
+      }}>
+        <button type="submit">Sign out</button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
--- auth.ts
+++ auth.ts
@@ -1,10 +1,26 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { db } from "./lib/db"
- 
-export const { auth, handlers: { GET, POST }, signIn, signOut } = NextAuth({
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db";
+
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
-  ...authConfig
-})
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log({ token });
+      
+      return token;
+    },
+    async session({ session, token }) {
+      console.log({ sessionToken: token });
+      return session;
+    },
+  },
+  ...authConfig,
+});
